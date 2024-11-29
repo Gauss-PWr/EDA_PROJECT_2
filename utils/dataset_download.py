fab9b3797773d9d32d7382ef8612a864108aa1c8
KacSamulski <163926867+KacSamulski@users.noreply.github.com>
2024-11-29 17:57:50 +0100

    Updated dataset_download.py accordingly to pull request's review

--- utils/dataset_download.py
+++ utils/dataset_download.py
@@ -1,19 +1,22 @@
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-link = 'https://www.kaggle.com/datasets/parisrohan/credit-score-classification' #just for convinience, not important
-download_path = "./data"
-dataset_name = "parisrohan/credit-score-classification"
-
-def download_dataset():
-    """create 'data' folder if it doesnt exist"""
-    os.makedirs(download_path, exist_ok=True)
+def download_dataset() -> bool:     #function will return True bool if operation is ended succesfuly
+    """function for downloading a dataset from kaggle"""
     
-    """activating kaggle api"""
+    link = 'https://www.kaggle.com/datasets/parisrohan/credit-score-classification' #just for convinience, not important
+    dataset_name = "parisrohan/credit-score-classification"
+
+    #reate 'data' folder if it doesnt exist
+    root_dir = os.path.dirname(os.path.dirname(__file__))
+    data_folder_path = os.path.join(root_dir, 'data')
+    os.makedirs(data_folder_path, exist_ok=True)
+    
+    #activating kaggle api
     api = KaggleApi()
     api.authenticate()
     
-    """self-explainatory"""
-    api.dataset_download_files(dataset_name, path=download_path, unzip=True)
-
-download_dataset()
+    #self-explainatory
+    api.dataset_download_files(dataset_name, path=data_folder_path, unzip=True)
+    
+    return True
